141fc47c5d453414be140eb04394f36e99ce80d9
Connor Roos <croos@nvidia.com>
2025-11-04 05:39:09 +0000

    Switch SAI to SAI Client in SAI sai_api_initialize
    Signed-off-by: Connor Roos <croos@nvidia.com>

--- orchagent/saihelper.cpp
+++ orchagent/saihelper.cpp
@@ -191,6 +191,7 @@ void initSaiApi()
         gProfileMap[SAI_REDIS_KEY_CONTEXT_CONFIG] = CONTEXT_CFG_FILE;
     }
 
+    gProfileMap[SAI_REDIS_KEY_ENABLE_CLIENT] = "true";
     sai_api_initialize(0, (const sai_service_method_table_t *)&test_services);
 
     sai_api_query(SAI_API_SWITCH,               (void **)&sai_switch_api);
